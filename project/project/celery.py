e33f236d092143702fbc503c6e927841981bc45d
alright0 <laright0@gmail.com>
2021-08-22 17:03:40 +0300

    initial commit

---
+++ project/project/celery.py
@@ -0,0 +1,28 @@
+import os
+
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+app = Celery("project")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+# пример расписания
+""" app.conf.beat_schedule = {
+    "debug_task": {"task": "project.celery.debug_task", "schedule": 15.0},
+} """
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
