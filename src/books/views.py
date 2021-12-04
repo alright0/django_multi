6cb40170d187241e5aec3c4130e1a177b436323e
alright0 <laright0@gmail.com>
2021-12-04 08:55:46 +0300

    still rest endpoints

--- src/books/views.py
+++ src/books/views.py
@@ -24,5 +24,20 @@ class BookView(GenericAPIView):
             serializer.save()
 
             response_json = {"message": "ok"}
-            return Response(response_json, status=status.HTTP_200_OK)
+            return Response(response_json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        id = request.data.get("id", 0)
+        book_for_delete = Book.objects.filter(id=id).first()
+
+        if book_for_delete:
+            title, author = book_for_delete.title, book_for_delete.author
+            book_for_delete.delete()
+
+            response_json = {"message": f"book: {author} - {title} deleted"}
+            return Response(response_json, status=status.HTTP_200_OK)
+        return Response(
+            {"message": f"book with id {id} not found"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
